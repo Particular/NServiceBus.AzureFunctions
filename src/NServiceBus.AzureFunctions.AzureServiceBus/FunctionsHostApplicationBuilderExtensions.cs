eaf408cf4f3a16cbc5c2f470cb0a07a00750b727
Matt Mercurio <mattmercurio@users.noreply.github.com>
2026-02-10 14:08:14 -0600

    AddNServiceBusFunction api drop AzureServiceBusServerlessTransport as param

--- src/NServiceBus.AzureFunctions.AzureServiceBus/FunctionsHostApplicationBuilderExtensions.cs
+++ src/NServiceBus.AzureFunctions.AzureServiceBus/FunctionsHostApplicationBuilderExtensions.cs
@@ -1,39 +1,31 @@
 namespace NServiceBus;
 
 using System;
-using System.Runtime.CompilerServices;
 using AzureFunctions.AzureServiceBus;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
+using Settings;
 
 public static class FunctionsHostApplicationBuilderExtensions
 {
     public static void AddNServiceBusFunction(
         this FunctionsApplicationBuilder builder,
         string endpointName,
-        AzureServiceBusServerlessTransport transport,
         Action<EndpointConfiguration> configure)
     {
-        ArgumentNullException.ThrowIfNull(transport);
-
-        var transportKey = $"NServiceBus.Transport.{RuntimeHelpers.GetHashCode(transport)}";
-        if (builder.Properties.TryGetValue(transportKey, out var existingEndpoint))
-        {
-            throw new InvalidOperationException(
-                $"This transport instance is already used by endpoint '{existingEndpoint}'. Each endpoint requires its own transport instance.");
-        }
-        builder.Properties[transportKey] = endpointName;
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(endpointName);
+        ArgumentNullException.ThrowIfNull(configure);
 
         builder.Services.AddAzureClientsCore();
 
-        builder.AddNServiceBusEndpoint(endpointName, endpoint =>
-        {
-            endpoint.UseTransport(transport);
-            configure(endpoint);
-        });
+        builder.AddNServiceBusEndpoint(endpointName, configure);
 
         builder.Services.AddKeyedSingleton<IMessageProcessor>(endpointName, (sp, _) =>
-            new MessageProcessor(transport, sp.GetRequiredKeyedService<MultiHosting.EndpointStarter>(endpointName)));
+        {
+            var transport = sp.GetRequiredKeyedService<IReadOnlySettings>(endpointName).Get<AzureServiceBusServerlessTransport>();
+            return new MessageProcessor(transport, sp.GetRequiredKeyedService<MultiHosting.EndpointStarter>(endpointName));
+        });
     }
 }

3d7e9ca81481d365cc2c9cb00dd319b40c9708ab
Andreas Öhlund <andreas.ohlund@particular.net>
2026-02-14 07:29:33 +0100

    Consolidate config

--- src/IntegrationTest.Shared/EndpointConfigBase.cs
+++ src/IntegrationTest.Shared/EndpointConfigBase.cs
@@ -2,11 +2,5 @@ namespace IntegrationTest.Shared;
 
 public abstract class EndpointConfigBase : IEndpointConfiguration
 {
-    public virtual void Configure(EndpointConfiguration configuration)
-    {
-        configuration.UseTransport(new AzureServiceBusServerlessTransport(TopicTopology.Default));
-        configuration.EnableInstallers();
-        configuration.UsePersistence<LearningPersistence>();
-        configuration.UseSerialization<SystemJsonSerializer>();
-    }
+    public virtual void Configure(EndpointConfiguration configuration) => CommonEndpointConfig.Apply(configuration);
 }

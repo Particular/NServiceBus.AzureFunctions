1a60e2b1865780f8b2124161cbe45812788e9ef4
David Boike <david.boike@gmail.com>
2026-02-17 13:39:08 -0600

    Don't register behaviors with DI

--- src/IntegrationTest/Infrastructure/TestStorageFeature.cs
+++ src/IntegrationTest/Infrastructure/TestStorageFeature.cs
@@ -9,10 +9,8 @@ public class TestStorageFeature : Feature
     {
         var endpointName = context.Settings.EndpointName();
         context.Services.AddSingleton(provider => new TestStorage(endpointName, provider.GetRequiredService<GlobalTestStorage>()));
-        context.Services.AddSingleton<IncomingTestBehavior>();
-        context.Services.AddSingleton<OutgoingTestBehavior>();
 
-        context.Pipeline.Register("IncomingTestBehavior", p => p.GetRequiredService<IncomingTestBehavior>(), "Log received messages");
-        context.Pipeline.Register("OutgoingTestBehavior", p => p.GetRequiredService<OutgoingTestBehavior>(), "Forward test case name to outgoing messages");
+        context.Pipeline.Register(typeof(IncomingTestBehavior), "Log received messages");
+        context.Pipeline.Register(typeof(OutgoingTestBehavior), "Forward test case name to outgoing messages");
     }
 }

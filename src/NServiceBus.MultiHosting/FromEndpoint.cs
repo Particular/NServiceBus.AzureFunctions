bd33b3afbc36c625d25ef378def275f3b24f7142
Andreas Öhlund <andreas.ohlund@particular.net>
2026-02-23 12:47:57 +0100

    Use keyed services

--- src/NServiceBus.MultiHosting/FromEndpoint.cs
+++
@@ -1,6 +0,0 @@
-namespace NServiceBus;
-
-using Microsoft.Extensions.DependencyInjection;
-
-[AttributeUsage(AttributeTargets.Parameter)]
-public class FromEndpoint(string endpointName) : FromKeyedServicesAttribute(endpointName);

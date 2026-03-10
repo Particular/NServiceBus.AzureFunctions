343a73679087e25c6fcd5b02ddb3986dfb9a4d50
Matt Mercurio <mattmercurio@users.noreply.github.com>
2026-03-10 16:46:08 -0500

    Move shared function types to the common project

--- src/NServiceBus.AzureFunctions/NServiceBusFunctionsInfrastructure.cs
+++ src/NServiceBus.AzureFunctions/NServiceBusFunctionsInfrastructure.cs
@@ -2,13 +2,12 @@ namespace NServiceBus;
 
 using System.ComponentModel;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Extensions.Azure;
 
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class NServiceBusFunctionsInfrastructure
 {
     public static void Initialize(FunctionsApplicationBuilder builder)
     {
-        builder.Services.AddAzureClientsCore();
+        ArgumentNullException.ThrowIfNull(builder);
     }
 }

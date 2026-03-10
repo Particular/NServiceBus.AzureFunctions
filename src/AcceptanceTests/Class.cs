bf0d4180e1661ce8a5413256c4fae293ae8f69a7
David Boike <david.boike@gmail.com>
2026-03-10 10:33:30 -0500

    Testing basics

--- src/AcceptanceTests/Class.cs
+++ src/AcceptanceTests/Class.cs
@@ -1,3 +1,12 @@
 namespace NServiceBus.AzureFunctions.AcceptanceTests;
 
+using NUnit.Framework;
 
+public class PlaceholderTest
+{
+    [Test]
+    public void Placeholder()
+    {
+        Assert.Pass("Just a placeholder so CI doesn't fail due to no tests found");
+    }
+}

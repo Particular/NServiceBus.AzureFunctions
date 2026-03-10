9eea81de6a8c759518564f750f95585729bf3490
David Boike <david.boike@gmail.com>
2026-03-10 10:48:13 -0500

    Add integration test contracts and infrastructure

---
+++ src/IntegrationTest.Contracts/TestingContracts.cs
@@ -0,0 +1,6 @@
+﻿namespace IntegrationTest.Contracts;
+
+public record ExceptionInfo(string FunctionName, string Type, string Message, string StackTrace);
+public record InfoResult(string Version, TimeSpan Uptime);
+public record MessageReceived(string MessageType, int Order, string SendingEndpoint, string ReceivingEndpoint);
+public record Payload(MessageReceived[] MessagesReceived);

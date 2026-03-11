e741272ab26a3dab8a5c8401f347aebc7c2e3425
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-03-11 17:49:49 +0100

    With polyshapr we don't need init and range actually

--- src/NServiceBus.AzureFunctions.Analyzer/CompilationAssemblyDetails.cs
+++ src/NServiceBus.AzureFunctions.Analyzer/CompilationAssemblyDetails.cs
@@ -1,15 +1,14 @@
-#nullable enable
 namespace NServiceBus.AzureFunctions.Analyzer;
 
 using Microsoft.CodeAnalysis;
-using NServiceBus.AzureFunctions.Analyzer.Utility;
+using Utility;
 
-record struct CompilationAssemblyDetails(string SimpleName, string Identity)
+readonly record struct CompilationAssemblyDetails(string SimpleName, string Identity)
 {
     public static CompilationAssemblyDetails FromAssembly(IAssemblySymbol assembly) => new(assembly.Name, assembly.Identity.GetDisplayName());
 
     const string NamePrefix = "GeneratedFunctionRegistrations_";
-    public readonly string ToGenerationClassName()
+    public string ToGenerationClassName()
     {
         var hash = NonCryptographicHash.GetHash(Identity);
         return $"{NamePrefix}{SimpleName.Replace('.', '_')}_{hash:x16}";
--- src/NServiceBus.AzureFunctions.Analyzer/Utility/IsExternalInit.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    // Enables init-only setters in netstandard2.0 project
-    static class IsExternalInit { }
-}

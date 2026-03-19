0975e2b4314759cb95ee66e7f6c582cc6f90810b
Andreas Öhlund <andreas.ohlund@particular.net>
2026-03-19 13:35:18 +0100

    Add failing test

--- src/Tests.Analyzers/FunctionEndpointGeneratorTests.cs
+++ src/Tests.Analyzers/FunctionEndpointGeneratorTests.cs
@@ -15,10 +15,11 @@ public class FunctionEndpointGeneratorTests
             .SuppressCompilationErrors()
             .Approve();
 
-    [TestCase(FunctionClassMustBePartial, "NSBFUNC001")]
-    [TestCase(FunctionClassShouldNotImplementIHandleMessages, "NSBFUNC002")]
-    [TestCase(FunctionMethodMustBePartial, "NSBFUNC003")]
-    [TestCase(MultipleConfigureMethods, "NSBFUNC005")]
+    [TestCase(FunctionClassMustBePartial, DiagnosticIds.ClassMustBePartial)]
+    [TestCase(FunctionClassShouldNotImplementIHandleMessages, DiagnosticIds.ShouldNotImplementIHandleMessages)]
+    [TestCase(FunctionMethodMustBePartial, DiagnosticIds.MethodMustBePartial)]
+    [TestCase(MultipleConfigureMethods, DiagnosticIds.MultipleConfigureMethods)]
+    [TestCase(MissingAutoComplete, DiagnosticIds.MissingAutoComplete)]
     public void ReportsGeneratorDiagnostics(string source, string diagnosticId)
     {
         var result = SourceGeneratorTest.ForIncrementalGenerator<FunctionEndpointGenerator>()
@@ -125,6 +126,25 @@ public class FunctionEndpointGeneratorTests
            public static void ConfigureProcessOrder(
                EndpointConfiguration endpointConfiguration,
                IConfiguration iconfiguration)
+           {
+           }
+       }
+       """;
+
+    const string MissingAutoComplete = """
+       namespace Demo;
+
+       public partial class Functions
+       {
+           [NServiceBusFunction]
+           [Function("ProcessOrder")]
+           public partial Task Run(
+               [ServiceBusTrigger("sales-queue", Connection = "AzureServiceBus")] ServiceBusReceivedMessage message,
+               ServiceBusMessageActions messageActions,
+               FunctionContext context,
+               CancellationToken cancellationToken);
+
+           public static void ConfigureProcessOrder(EndpointConfiguration endpointConfiguration)
            {
            }
        }

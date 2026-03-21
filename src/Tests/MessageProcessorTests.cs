a2dc0de9f1f656394ce4db71b1bd587ca3376468
Andreas Öhlund <andreas.ohlund@particular.net>
2026-03-21 06:36:00 +0100

    Consolidate test code

--- src/Tests/MessageProcessorTests.cs
+++ src/Tests/MessageProcessorTests.cs
@@ -12,65 +12,73 @@ public class MessageProcessorTests
     [Test]
     public async Task ShouldCallCompleteWhenOnMessageSucceeds()
     {
-        var processor = new PipelineInvokingMessageProcessor(new FakeBaseReceiver());
-
-        var onErrorWasCalled = false;
-        var onMessageWasCalled = false;
-        await processor.Initialize(PushRuntimeSettings.Default,
-            (_, _) =>
-            {
-                onMessageWasCalled = true;
-                return Task.CompletedTask;
-            }, (_, _) =>
-            {
-                onErrorWasCalled = true;
-                return Task.FromResult(ErrorHandleResult.Handled);
-            });
-
-        var serviceBusReceivedMessage = ServiceBusModelFactory.ServiceBusReceivedMessage();
-        var messageActions = new TestableMessageActions();
-
-        await processor.Process(serviceBusReceivedMessage, messageActions);
+        var result = await ProcessMessage(
+            onMessage: _ => Task.CompletedTask);
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.IsTrue(onMessageWasCalled);
-            Assert.IsFalse(onErrorWasCalled);
-            Assert.IsTrue(messageActions.WasCompleted);
-            Assert.IsFalse(messageActions.WasAbandoned);
+            Assert.IsTrue(result.OnMessageWasCalled);
+            Assert.IsFalse(result.OnErrorWasCalled);
+            Assert.IsTrue(result.MessageActions.WasCompleted);
+            Assert.IsFalse(result.MessageActions.WasAbandoned);
         }
     }
 
     [Test]
     public async Task ShouldCallAbandonWhenOnMessageFailsAndRetryIsRequested()
     {
-        var processor = new PipelineInvokingMessageProcessor(new FakeBaseReceiver());
-
-        var onErrorWasCalled = false;
-        var onMessageWasCalled = false;
-        await processor.Initialize(PushRuntimeSettings.Default,
-            (_, _) =>
-            {
-                onMessageWasCalled = true;
-                throw new Exception("simulated exception");
-            }, (_, _) =>
-            {
-                onErrorWasCalled = true;
-                return Task.FromResult(ErrorHandleResult.RetryRequired);
-            });
-
-        var serviceBusReceivedMessage = ServiceBusModelFactory.ServiceBusReceivedMessage();
-        var messageActions = new TestableMessageActions();
-
-        await processor.Process(serviceBusReceivedMessage, messageActions);
+        var result = await ProcessMessage(
+            onMessage: _ => throw new Exception("simulated exception"));
 
         using (Assert.EnterMultipleScope())
         {
-            Assert.IsTrue(onMessageWasCalled);
-            Assert.IsTrue(onErrorWasCalled);
-            Assert.IsFalse(messageActions.WasCompleted);
-            Assert.IsTrue(messageActions.WasAbandoned);
+            Assert.IsTrue(result.OnMessageWasCalled);
+            Assert.IsTrue(result.OnErrorWasCalled);
+            Assert.IsFalse(result.MessageActions.WasCompleted);
+            Assert.IsTrue(result.MessageActions.WasAbandoned);
         }
+    }
+
+#pragma warning disable CS8425 // Func used as a method parameter with a Task return type argument should have at least one CancellationToken parameter type argument
+#pragma warning disable PS0013
+    async Task<ProcessingResult> ProcessMessage(
+        ServiceBusReceivedMessage? message = null,
+        Func<MessageContext, Task>? onMessage = null,
+        Func<ErrorContext, Task<ErrorHandleResult>>? onError = null)
+    {
+        message ??= ServiceBusModelFactory.ServiceBusReceivedMessage();
+        onMessage ??= _ => Task.CompletedTask;
+        onError ??= _ => Task.FromResult(ErrorHandleResult.RetryRequired);
+
+        var processor = new PipelineInvokingMessageProcessor(new FakeBaseReceiver());
+        bool onMessageWasCalled = false;
+        bool onErrorWasCalled = false;
+
+        var messageActions = new TestableMessageActions();
+
+        await processor.Initialize(PushRuntimeSettings.Default,
+            async (msgContext, _) =>
+            {
+                onMessageWasCalled = true;
+                await onMessage(msgContext);
+            },
+            async (errorContext, _) =>
+            {
+                onErrorWasCalled = true;
+                return await onError(errorContext);
+            });
+
+        await processor.Process(message, messageActions);
+        return new ProcessingResult(onMessageWasCalled, onErrorWasCalled, messageActions);
+    }
+#pragma warning restore PS0013
+#pragma warning restore CS8425
+
+    class ProcessingResult(bool onMessageWasCalled, bool onErrorWasCalled, TestableMessageActions messageActions)
+    {
+        public bool OnMessageWasCalled { get; } = onMessageWasCalled;
+        public bool OnErrorWasCalled { get; } = onErrorWasCalled;
+        public TestableMessageActions MessageActions { get; } = messageActions;
     }
 
     class TestableMessageActions : ServiceBusMessageActions
@@ -94,16 +102,12 @@ public class MessageProcessorTests
 
     class FakeBaseReceiver : IMessageReceiver
     {
-        public Task Initialize(PushRuntimeSettings limitations, OnMessage onMessage, OnError onError, CancellationToken cancellationToken = new CancellationToken()) => Task.CompletedTask;
-
-        public Task StartReceive(CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
-
-        public Task ChangeConcurrency(PushRuntimeSettings limitations, CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
-
-        public Task StopReceive(CancellationToken cancellationToken = new CancellationToken()) => throw new NotImplementedException();
-
-        public ISubscriptionManager Subscriptions { get; }
-        public string Id { get; }
-        public string ReceiveAddress { get; } = "TestEndpoint";
+        public Task Initialize(PushRuntimeSettings limitations, OnMessage onMessage, OnError onError, CancellationToken cancellationToken = new()) => Task.CompletedTask;
+        public Task StartReceive(CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+        public Task ChangeConcurrency(PushRuntimeSettings limitations, CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+        public Task StopReceive(CancellationToken cancellationToken = new()) => throw new NotImplementedException();
+        public ISubscriptionManager Subscriptions => null!;
+        public string Id => string.Empty;
+        public string ReceiveAddress => "TestEndpoint";
     }
 }

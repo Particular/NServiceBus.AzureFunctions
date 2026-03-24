f5ce9de279e3a9dbc453408ffc86088bc8436a2b
Andreas Öhlund <andreas.ohlund@particular.net>
2026-03-24 14:50:10 +0100

    Revert test code

--- src/IntegrationTest.Sales/Handlers/SubmitOrderHandler.cs
+++ src/IntegrationTest.Sales/Handlers/SubmitOrderHandler.cs
@@ -8,10 +8,8 @@ public class SubmitOrderHandler(ILogger<SubmitOrderHandler> logger, MyComponent 
 {
     public async Task Handle(SubmitOrder message, IMessageHandlerContext context)
     {
-//        logger.LogWarning($"Handling {nameof(SubmitOrder)} in {nameof(SubmitOrderHandler)} with component for {component.EndpointName}");
-//
-//        await context.Publish(new OrderSubmitted()).ConfigureAwait(false);
+        logger.LogWarning($"Handling {nameof(SubmitOrder)} in {nameof(SubmitOrderHandler)} with component for {component.EndpointName}");
 
-        throw new Exception("Simulated exception");
+        await context.Publish(new OrderSubmitted()).ConfigureAwait(false);
     }
 }

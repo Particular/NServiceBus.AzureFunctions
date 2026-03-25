6d632690453a914e3863a78e5157486c275d2541
Andreas Öhlund <andreas.ohlund@particular.net>
2026-03-25 14:35:29 +0100

    Formatting

--- src/Tests/DeadLetterMessageTests.cs
+++ src/Tests/DeadLetterMessageTests.cs
@@ -44,7 +44,6 @@ public class DeadLetterMessageTests
         void SimulateException()
         {
             throw new InvalidOperationException("Test exception");
-            ;
         }
     }
 

b7a3ff34c041a55d9467e7c225fafd09ea26c52e
David Boike <david.boike@gmail.com>
2026-04-15 11:06:28 -0500

    Add packaging using 2-package strategy (#56)
    * Rename common project and set up packaging
    
    * Define Roslyn version as 5 (which is already minimum for NServiceBus 10)
    
    * Add approval test for base Functions project
    
    * Temporarily allow CI to pass with XML documentation warnings present
    
    * Move the analyzers to the common package
    
    * Don't appear to need that package anyway
    
    * Moving analyzer moved where this was generated, but honestly I'm not even sure if this is right, could be analyzer bug
    
    * Scrubber in the wrong place too
    
    * Neither of the packed components should reference (invoke) the source generators
    
    * Result of removing analyzer reference from ASB component
    
    * Partial is not needed since the GeneratedRegex came out

--- src/Tests/ApiApprovals.cs
+++ src/Tests/ApiApprovals.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.AzureFunctions.Tests;
 
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 using Particular.Approvals;
 using PublicApiGenerator;
@@ -8,9 +9,20 @@ using PublicApiGenerator;
 public class ApiApprovals
 {
     [Test]
-    public void ApprovaAzureServiceBusComponentApi()
+    public void ApproveAzureServiceBusComponentApi()
     {
         var publicApi = typeof(AzureServiceBusServerlessTransport).Assembly.GeneratePublicApi(new ApiGeneratorOptions()
+        {
+            ExcludeAttributes = ["System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute"]
+        });
+
+        Approver.Verify(publicApi);
+    }
+
+    [Test]
+    public void ApproveFunctionsComponentApi()
+    {
+        var publicApi = typeof(NServiceBusFunctionAttribute).Assembly.GeneratePublicApi(new ApiGeneratorOptions()
         {
             ExcludeAttributes = ["System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute"]
         });

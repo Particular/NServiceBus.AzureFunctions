fc79311295a9032719bd850cfa3eae7767461de0
Andreas Öhlund <andreas.ohlund@particular.net>
2026-06-04 13:39:08 +0200

    Add failing NSBFUNC004 test

--- src/Tests.Analyzers/MissingCompositionCallAnalyzerTests.cs
+++ src/Tests.Analyzers/MissingCompositionCallAnalyzerTests.cs
@@ -1,7 +1,13 @@
 namespace NServiceBus.AzureFunctions.Analyzers.Tests;
 
+using System.IO;
+using System.Linq;
+using Microsoft.Azure.Functions.Worker;
 using NServiceBus.AzureFunctions.Analyzer;
+using NServiceBus.AzureFunctions.Analyzer.Utility;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
 using NUnit.Framework;
 using Particular.AnalyzerTesting;
 
@@ -78,6 +84,33 @@ public class MissingCompositionCallAnalyzerTests
         Assert.That(diagnostics, Has.None.Matches<Diagnostic>(d => d.Id == DiagnosticIds.MissingAddNServiceBusFunctionsCall));
     }
 
+    [Test]
+    public async Task DoesNotReportDiagnosticForNonFunctionsProjectReferencingFunctionsApp()
+    {
+        var functionsAppReference = BuildFunctionsAppReference();
+
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "Tests",
+            syntaxTrees:
+            [
+                CSharpSyntaxTree.ParseText(
+                    "public class Tests { }",
+                    path: "Tests.cs")
+            ],
+            references:
+            [
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(FunctionAttribute).Assembly.Location),
+                functionsAppReference
+            ],
+            options: new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+        var analyzer = new MissingCompositionCallAnalyzer();
+        var diagnostics = await compilation.WithAnalyzers([analyzer]).GetAnalyzerDiagnosticsAsync();
+
+        Assert.That(diagnostics, Has.None.Matches<Diagnostic>(d => d.Id == DiagnosticIds.MissingAddNServiceBusFunctionsCall));
+    }
+
     static SourceGeneratorTest CreateSourceGeneratorAnalyzerTest() =>
         SourceGeneratorTest.ForIncrementalGenerator<FunctionEndpointGenerator>()
             .WithIncrementalGenerator<SendOnlyEndpointGenerator>()
@@ -88,4 +121,41 @@ public class MissingCompositionCallAnalyzerTests
             .WithProperty("build_property.FunctionsExecutionModel", "isolated")
             .WithProperty("build_property.RootNamespace", "My.FunctionApp")
             .SuppressCompilationErrors();
+
+    static MetadataReference BuildFunctionsAppReference()
+    {
+        const string assemblyName = "ReferencedFunctionsApp";
+
+        var preliminaryCompilation = CSharpCompilation.Create(
+            assemblyName: assemblyName,
+            references:
+            [
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+            ],
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generatedTypeName = $"GeneratedFunctionRegistrations_{assemblyName}_{NonCryptographicHash.GetHash(preliminaryCompilation.Assembly.Identity.GetDisplayName()):x16}";
+
+        var compilation = CSharpCompilation.Create(
+            assemblyName: "ReferencedFunctionsApp",
+            syntaxTrees:
+            [
+                CSharpSyntaxTree.ParseText(
+                    $"namespace NServiceBus.Generated; public static class {generatedTypeName} {{ }}",
+                    path: "Generated.cs")
+            ],
+            references:
+            [
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+            ],
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        using var stream = new MemoryStream();
+        var emitResult = compilation.Emit(stream);
+
+        Assert.That(emitResult.Success, Is.True, string.Join(System.Environment.NewLine, emitResult.Diagnostics.Select(static d => d.ToString())));
+
+        stream.Position = 0;
+        return MetadataReference.CreateFromImage(stream.ToArray());
+    }
 }
